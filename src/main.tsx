24bb74b86909b6b91591d3987e9867bdc0792a4f
Adi pranoto <miuprix@gmail.com>
2023-09-01 07:05:01 +0000

    change default chain

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { WagmiConfig } from 'wagmi'
 import { chains, config } from './wagmiConfig.tsx' // Untuk mengkonfigurasi wagmi, semua ada di module ini
-import { bscTestnet } from 'wagmi/chains'
+import { sepolia } from 'wagmi/chains'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <RainbowKitProvider
               // Atur behavior dari RainbowKitProvider
               chains={chains}
-              initialChain={bscTestnet}
+              initialChain={sepolia}
               modalSize='compact'
               showRecentTransactions={true}
           >
