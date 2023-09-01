0cf19796be61c4187ce74b215a1ce07b7fdb4670
Adi pranoto <miuprix@gmail.com>
2023-09-01 09:14:12 +0000

    Saldo

--- src/components/MyBalance.tsx
+++ src/components/MyBalance.tsx
@@ -3,11 +3,19 @@ import '../style/style.css'
 
 function MyBalance() {
     const { data, isError, isLoading } = useBalance({
-        address: '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+        address: "0x222374582e5015621D648e1E2A17A431b8b9d4Db",
+        formatUnits: 'ether',
+        
+        //  Optional, jika property token di hilangkan akan menampilkan saldo ether
+        token: "0xbCa7404Bd54Dd2AF299eF828613cb4cb6D0df9d5",
+        watch: true,
+        onError(error){
+            console.log('Error', error)
+        }
     })
 
-    if (isLoading) return <div>Fetching balance…</div>
-    if (isError) return <div>Error fetching balance</div>
+    if (isLoading) return <div className="card">Fetching balance…</div>
+    if (isError) return <div className="card">Error fetching balance</div>
 
     return(
         <div className="card">
