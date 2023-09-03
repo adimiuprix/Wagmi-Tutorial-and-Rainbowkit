d94497d6a038a73a9ae82953d004cd77616695c1
Adi pranoto <miuprix@gmail.com>
2023-09-03 06:31:52 +0000

    Memberi komentar

--- src/components/BacaToken.tsx
+++ src/components/BacaToken.tsx
@@ -1,8 +1,14 @@
-import { useToken } from "wagmi";
+/* Menggunakan hook useToken dapat di gunakan untuk menampilkan informasi standard erc20
+   Informasi standard seperti: name, symbol, decimal, total supply */
+import { useToken } from "wagmi";   
 
 function BacaToken(){
     const { data, isError, isLoading } = useToken({
+        /* Informasi hanya baca dan di render
+        kita tidak memerlukan ABI-JSON
+        hanya alamat token pada jaringan */
         address: "0xcd43dc81ebbe592be94c67ab8a09420ecb0fb6aa",
+        // sediakan property setelah address tapi optional saja
     })
 
     if (isLoading) return (<div className="card">Fetching token…</div>)
