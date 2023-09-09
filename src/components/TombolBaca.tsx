9a0e9d0dbb899c661d9903e958c11ad39c9113e9
Adi pranoto <miuprix@gmail.com>
2023-09-09 00:50:14 +0000

    Membuat tombol menampilkan data

---
+++ src/components/TombolBaca.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { useContractRead } from 'wagmi';
+import { IngatanContract } from "../constant/IngatanContract";
+
+function TombolBaca(){
+    const [showMessage, setShowMessage] = useState(false); // State untuk mengontrol tampilan pesan
+    const { data: IsiPesan, isError, isLoading } = useContractRead({
+        address: "0x6302cD20aC525DaF11E387Cb6B960DF9ec8494a7",
+        abi: IngatanContract,
+        functionName: "TampilkanData",
+        watch: true,
+    })
+
+    const handleClick = () => {
+        setShowMessage(true); // Saat tombol diklik, tampilkan pesan
+    };
+
+    // Bagian kondisi "if" itu opsional, bisa dihapus jika tidak diperlukan
+    if (isLoading) {
+        return <div className="card">Loading...</div>;
+    }
+    if (isError) {
+        return <div className="card">Error reading contract data.</div>;
+    }
+
+    return(
+        <div className="card">
+            <button onClick={handleClick}>Tampilkan Pesan</button>
+            {showMessage && <p>Pesan dari contract: {IsiPesan}</p>}
+        </div>
+    )
+}
+
+export default TombolBaca
---
+++ src/constant/IngatanContract.ts
@@ -0,0 +1,33 @@
+export const IngatanContract = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "Ingatan",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "TampilkanData",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ] as const
