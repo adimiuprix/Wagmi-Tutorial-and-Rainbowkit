2149375f8ebb4b60dcf3a368eb0e9ea1ddb010ce
Adi pranoto <miuprix@gmail.com>
2023-09-10 10:17:31 +0000

    hapus n satu

--- src/components/Approval.tsx
+++ src/components/Approval.tsx
@@ -3,18 +3,18 @@ import { useContractWrite, erc20ABI } from 'wagmi'
 import { ApproveContract } from '../constant/ApproveContract'
 
 function Approval () {
-    // Membuat 2 fungsi yang akan diajalankan, pertama melakukan approval kemudia deposit
-
-    // Membuat fungsi untuk melakukan approve tokennya dulu
-    // sebelum smart contract external di izinkan
-    // Argumentnya adalah "spender" dan "value"
+    /* Membuat 2 fungsi yang akan diajalankan, pertama melakukan approval kemudia deposit
+ 
+    Membuat fungsi untuk melakukan approve tokennya dulu
+    sebelum smart contract external di izinkan
+    Argumentnya adalah "spender" dan "value" */
     const { write: approveWrite } = useContractWrite({
         address: '0xCd43dC81ebbe592Be94C67AB8A09420ecB0fB6Aa',  // Address token nya
         abi: erc20ABI,
         functionName: "approve",
         args: [ '0x70213f1A5E2D146Db7Cb479C5651CB7112f52454', (parseEther('1')) ],  // Izinkan alamat spender dan masukkan nilai
 		
-        //  Jika berhasil jalannkan fungsi kedua yang bernama deposits
+        //  Jika berhasil jalankan fungsi kedua yang bernama deposits
         onSuccess() {
 			setTimeout( () => {
 				deposit?.()
