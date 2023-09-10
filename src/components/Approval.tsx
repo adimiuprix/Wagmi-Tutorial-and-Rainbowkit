4d6134736e64465f5fb0ef8dfcda16e9d33321ad
Adi pranoto <miuprix@gmail.com>
2023-09-10 10:14:04 +0000

    Approval final

--- src/components/Approval.tsx
+++ src/components/Approval.tsx
@@ -1,54 +1,42 @@
 import { parseEther } from 'viem'
-import { useContractWrite, useAccount, erc20ABI, useContractRead } from 'wagmi'
+import { useContractWrite, erc20ABI } from 'wagmi'
 import { ApproveContract } from '../constant/ApproveContract'
 
 function Approval () {
-    const { address } = useAccount()
+    // Membuat 2 fungsi yang akan diajalankan, pertama melakukan approval kemudia deposit
 
+    // Membuat fungsi untuk melakukan approve tokennya dulu
+    // sebelum smart contract external di izinkan
+    // Argumentnya adalah "spender" dan "value"
     const { write: approveWrite } = useContractWrite({
-        address: '0xCd43dC81ebbe592Be94C67AB8A09420ecB0fB6Aa',
+        address: '0xCd43dC81ebbe592Be94C67AB8A09420ecB0fB6Aa',  // Address token nya
         abi: erc20ABI,
         functionName: "approve",
-        args: [ '0x70213f1A5E2D146Db7Cb479C5651CB7112f52454', Number( parseEther( "1" ) ) ],
-		onSuccess() {
+        args: [ '0x70213f1A5E2D146Db7Cb479C5651CB7112f52454', (parseEther('1')) ],  // Izinkan alamat spender dan masukkan nilai
+		
+        //  Jika berhasil jalannkan fungsi kedua yang bernama deposits
+        onSuccess() {
 			setTimeout( () => {
 				deposit?.()
 			}, 7000 );
 		},
-
     })
 
+    // Jalankan fungsi deposits seperti biasanya
 	const { write: deposit } = useContractWrite( {
 		address: '0x70213f1A5E2D146Db7Cb479C5651CB7112f52454',
 		abi: ApproveContract,
 		functionName: "deposit",
-		account: address,
-		chainId: 97,
-		args: [ 0, Number( parseEther( "2" ) ), 10 ],
+		 args: [ (parseEther('1')) ],  //  Masukkan argument nya
 		onSuccess( data ) {
-			console.log( data );
-		},
-		onError( err ) {
-			console.log( err?.shortMessage );
+			console.log( data )
 		},
 	})
 
-	const { data: allowance } = useContractRead( {
-		address: '0xCd43dC81ebbe592Be94C67AB8A09420ecB0fB6Aa',
-		abi: erc20ABI,
-		functionName: "allowance",
-		chainId: 97,
-		args: [ "0xCd43dC81ebbe592Be94C67AB8A09420ecB0fB6Aa", "0x70213f1A5E2D146Db7Cb479C5651CB7112f52454" ],
-		watch: true,
-	} )
-
 	return (
-		<>
-			<div>
-				<span>Allowance : {Number( allowance )}</span>
-				<button onClick={() => approveWrite?.()}>Approve/Transfer</button>
+			<div className="card">
+				<button onClick={() => approveWrite?.()} className='calc-btn'>Deposit</button>
 			</div>
-		</>
 	)
 }
 
